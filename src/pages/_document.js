9c03afd82e607bb0d4c19adb82ccc16f6c999fd2
carsonwilcox <carson4wilcox@gmail.com>
2025-06-09 22:26:31 -0700

    Add SEO and social sharing tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,27 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
+        {/* Primary Meta Tags */}
+        <title>REFOOT™ Recovery Socks</title>
+        <meta name="title" content="REFOOT™ Recovery Socks" />
+        <meta name="description" content="Premium post-workout socks engineered for athletes who demand more than just cotton. Recover stronger. Train smarter." />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://refootwear.com" />
+        <meta property="og:title" content="REFOOT™ Recovery Socks" />
+        <meta property="og:description" content="Premium post-workout socks engineered for athletes who demand more than just cotton. Recover stronger. Train smarter." />
+        <meta property="og:image" content="https://refootwear.com/images/sock-black.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://refootwear.com" />
+        <meta property="twitter:title" content="REFOOT™ Recovery Socks" />
+        <meta property="twitter:description" content="Premium post-workout socks engineered for athletes who demand more than just cotton. Recover stronger. Train smarter." />
+        <meta property="twitter:image" content="https://refootwear.com/images/sock-black.png" />
+
         {/* Google Analytics */}
         <script
           async
@@ -21,6 +40,7 @@ export default function Document() {
             `,
           }}
         />
+
         <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
